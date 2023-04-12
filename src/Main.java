b2c8be78c58d05ca3d4a03dd2aa1b3cc0c52f800
Sergey Barkhalev <dr.barkhalev@yandex.ru>
2023-04-13 01:22:04 +0500

    Добавил в main min() и average() methods in salesManager class

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,12 @@ public class Main {
         int[] sales = new int[]{1,2,300,200};
         SalesManager salesManager = new SalesManager(sales);
         int max = salesManager.max();
+        int min = salesManager.min();
+        int average = salesManager.average();
+
         System.out.println(max);
+        System.out.println(min);
+        System.out.println(average);
 
     }
 }
